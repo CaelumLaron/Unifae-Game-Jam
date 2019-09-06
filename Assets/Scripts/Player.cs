0e8636bf3cefc7760aa1a1c1aa6906db7a9c820c
CaelumLaron <lucas.rocha@icomp.ufam.edu.br>
2019-09-06 14:47:09 -0400

    Player Improves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,55 +4,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+
 public class Player : MonoBehaviour{
 	
 	[Header("Movement Settings")]
 	[SerializeField] float walkSpeed = .3f;
 	[SerializeField] float groundRunSpeed = 8f;
 	[SerializeField] float airRunSpeed = 3f;
-	[SerializeField] float jumpHeight = 2f;
-	[SerializeField] float gravity = 9.8f; 
+	
+	[Header("Jump Settings")]
+	[SerializeField] float jumpForce = 10f;
+	[SerializeField] float jumpTime = 1f;
+
+	//Cached Objects
+	Rigidbody2D rg;
+	BoxCollider2D bx2d;
+	CircleCollider2D c2d;
 
 	//Cached Variable
-	BoxCollider2D bx2d;
-	Vector3 velocity;
-	bool standing = false, jumpInput, runInput;
-	float inputX, inputY, speed;
+	float inputX, jumpCounter;
 	bool stopPlayer = false;
+	bool standing = false, jumpInput, runInput, jumping = false;
 
 	void Start(){
-		velocity = Vector3.zero;
 		bx2d = GetComponent<BoxCollider2D>();
+		c2d = GetComponent<CircleCollider2D>();
+		rg = GetComponent<Rigidbody2D>();
 		Hazard.onDeath += EndGame;
 	}
 
-    void Update(){
-    	standing = bx2d.IsTouchingLayers(LayerMask.GetMask("Ground"));
-    	runInput = Input.GetKey(KeyCode.LeftShift);
+	void FixedUpdate(){
+		if(stopPlayer)
+    		return;
+		runInput = Input.GetKey(KeyCode.LeftShift);
     	inputX = Input.GetAxisRaw("Horizontal");
-    	speed = !standing? airRunSpeed : (runInput? groundRunSpeed : walkSpeed);
     	if(standing){
-    		inputY = .0f;
-    		jumpInput = Input.GetButton("Jump");
-    		if(jumpInput && !stopPlayer)
-    			inputY = Mathf.Sqrt(2 * gravity * jumpHeight);
+    		rg.velocity = new Vector2(inputX * (runInput? groundRunSpeed : walkSpeed), rg.velocity.y);
     	}
-    	else
-    		inputY -= (gravity * Time.deltaTime);
+		else{
+			if(Mathf.Abs(rg.velocity.x) > airRunSpeed){
+				rg.velocity = new Vector2(inputX * groundRunSpeed, rg.velocity.y);
+			}
+			else{
+				rg.velocity = new Vector2(inputX * airRunSpeed, rg.velocity.y);
+			}
+		}
+	}
+
+    void Update(){
     	if(stopPlayer)
-    		inputX = .0f;
-    	velocity = new Vector3(speed * inputX , inputY, .0f);   	
-    	transform.position += Time.deltaTime * velocity;
+    		return;
+    	standing = c2d.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    	if(standing && Input.GetButtonDown("Jump")){
+    		rg.velocity = Vector2.up * jumpForce;
+    		jumpCounter = jumpTime;
+    		jumping = true;
+    	}
+    	if(jumping && Input.GetButton("Jump")){
+    		if(jumpCounter > 0){
+    			rg.velocity = Vector2.up * jumpForce;
+    			jumpCounter -= Time.deltaTime;
+    		}
+    		else{
+    			jumping = false;
+    		}
+    	}
+    	if(Input.GetButtonUp("Jump")){
+    		jumping = false;
+    	}
+
     }
 
     void EndGame(){
     	Debug.Log("Player Died!");
     	stopPlayer = true;
+    	rg.velocity = Vector2.zero;
     	Hazard.onDeath -= EndGame;
     }
 
-    private void OnCollisionEnter2D(){
-    	
-    }
 }
